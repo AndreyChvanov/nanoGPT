c09d8f78fbd065d0c012d8f01ec6316f1a144492
AndreyChvanov <andryshow13@gmail.com>
2024-04-25 21:05:31 +0300

    upd

--- configurator.py
+++ configurator.py
@@ -29,6 +29,7 @@ for arg in sys.argv[1:]:
     else:
         # assume it's a --key=value argument
         assert arg.startswith('--')
+        print(arg.split('='))
         key, val = arg.split('=')
         print(key, val)
         key = key[2:]
